fe9f4e48763a12630301376cacdf26c995cf8e9b
Nigel Nelson <nigel@prodatakey.com>
2018-08-26 16:59:43 -0600

    add originX and originY property to sheets

--- src/components/Sprite.js
+++ src/components/Sprite.js
@@ -3,8 +3,8 @@ import { getDataUrl } from '../services/images';
 import './Sprite.css'
 
 export default ({ sheet, sprite, x, y, scale }) => {
-    const top = Math.floor(y)*scale;
-    const left = Math.floor(x)*scale;
+    const top = Math.floor(y - (sheet.originY || 0))*scale;
+    const left = Math.floor(x - (sheet.originX || 0))*scale;
     return (
         <img src={getDataUrl({sheet,sprite,scale})} alt="" style={{position:'absolute',top,left}}/>
     );
--- src/game/sheets.js
+++ src/game/sheets.js
@@ -8,17 +8,23 @@ export const sheets = [
         src: guysPng,
         spriteWidth: 16,
         spriteHeight: 16,
+        originX: 8,
+        originY: 8,
     },
     {
         name: 'ground',
         src: groundPng,
         spriteWidth: 4,
         spriteHeight: 8,
+        originX: 0,
+        originY: 0,
     },
     {
         name: 'props',
         src: propsPng,
         spriteWidth: 32,
         spriteHeight: 32,
+        originX: 16,
+        originY: 32,
     }
 ];
