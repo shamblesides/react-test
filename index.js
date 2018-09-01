dde0fc4147d6e6ad0085782ac58227e192d35dbd
Nigel Nelson <nigel@prodatakey.com>
2018-09-01 10:57:48 -0600

    add new index with multiple game instances

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Game } from './Game';
+
+export default () => (
+    <div>
+        <div style={{ width: '100%', height: 300 }}>
+            <Game />
+        </div>
+        <div style={{ width: '100%', height: 300 }}>
+            <Game />
+        </div>
+    </div>
+);
