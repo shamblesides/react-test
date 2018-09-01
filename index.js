e2a75d0f7fc3ab5a0d2320b14caad24276a57946
Nigel Nelson <nigel@prodatakey.com>
2018-08-31 23:53:37 -0600

    stop looping once game unmounts from the dom

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import { Screen, Loader, Pad } from './lib';
 import { gameloop, binds, sheets, ROOM_HEIGHT, ROOM_WIDTH } from './game';
 
 export default class App extends Component {
+    destroyed = false;
+
     registerPad = (pad) => {
         this.setState({ pad });
     }
 
     loop = () => {
+        if (this.destroyed) return;
         window.requestAnimationFrame(() => {
             this.setState({ ...gameloop(this.state) });
             this.loop();
@@ -17,6 +20,10 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = { sheets, binds, sprites: [] };
+    }
+
+    componentWillUnmount() {
+        this.destroyed = true;
     }
 
     render() {
