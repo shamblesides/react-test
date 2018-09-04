dbb001e4725c1b94f30ca38c983b33f7f25f7ffc
Nigel Nelson <nigel@prodatakey.com>
2018-09-03 18:54:10 -0600

    finish getting styles to work for screens filling or not filling containers

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ export default () => (
         <div class="some-index-container-1" style={{ width: '100%', height: 300, border: 'solid 1px' }}>
             <Game scale={'auto'} />
         </div>
-        <div class="some-index-container-2" style={{ width: '100%', height: 300, border: 'solid 1px' }}>
-            <Game scale={2} />
-            <Game scale={2} />
+        <div class="some-index-container-2" style={{ width: '100%', height: 300, border: 'solid 1px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ margin: '20px' }}>
+                <Game scale={2} />
+            </div>
+            <div style={{ margin: '20px' }}>
+                <Game scale={2} />
+            </div>
         </div>
     </div>
 );
