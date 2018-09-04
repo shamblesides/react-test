df1811716c77c8bf45a46506e7c2b875ff28bacb
Nigel Nelson <nigel@prodatakey.com>
2018-09-03 18:30:35 -0600

    better identifying class names on elements

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { Game } from './Game';
 
 export default () => (
-    <div>
-        <div style={{ width: '100%', height: 300 }}>
+    <div class="some-index-root">
+        <div class="some-index-container-1" style={{ width: '100%', height: 300, border: 'solid 1px' }}>
             <Game scale={'auto'} />
         </div>
-        <div style={{ width: '100%', height: 300 }}>
+        <div class="some-index-container-2" style={{ width: '100%', height: 300, border: 'solid 1px' }}>
             <Game scale={2} />
             <Game scale={2} />
         </div>
