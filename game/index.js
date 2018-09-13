d657386e9c7f6faed6f46365bc7bcc5c35606bdf
Nigel Nelson <nigel@prodatakey.com>
2018-09-12 21:54:17 -0600

    configure background color from within game, don't require binds

--- game/index.js
+++ game/index.js
@@ -2,5 +2,6 @@ import { binds } from './binds';
 import { gameloop } from './gameloop';
 import { sheets } from './sheets';
 import { ROOM_HEIGHT as height, ROOM_WIDTH as width } from './rooms';
+const backgroundColor = '#45283c';
 
-export default { binds, gameloop, sheets, height, width };
+export default { binds, gameloop, sheets, height, width, backgroundColor };
