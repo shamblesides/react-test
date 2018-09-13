bb880d6b2eec18f7f192cf5fcfc059fee9db7157
Nigel Nelson <nigel@prodatakey.com>
2018-09-13 10:42:22 -0600

    use webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,17 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [require('@babel/plugin-proposal-object-rest-spread')],
+                    },
+                },
+            },
         ],
     },
 };
