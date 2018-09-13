ff27d71cfb3ac6beeb52513440755a2e228148d5
Nigel Nelson <nigel@prodatakey.com>
2018-09-12 21:36:36 -0600

    the least important optimization imaginable

--- game/math.js
+++ game/math.js
@@ -1,12 +1,14 @@
-export function mid(...nums) {
-    nums.sort((a,b) => a-b);
-    const half = Math.floor(nums.length / 2);
-    if (nums.length % 2 === 0) {
-        return (nums[half - 1] + nums[half]) / 2;
-    }
-    return nums[half];
+export function mid(a, b, c) {
+    const x = a - b;
+    const y = b - c;
+    const z = a - c;
+    if (x * y > 0) return b;
+    if (x * z > 0) return c;
+    return a;
 }
 
 export function clamp(value, absMax) {
-    return mid(absMax, -absMax, value);
+    if (value <= -absMax) return -absMax;
+    if (value >= absMax) return absMax;
+    return value;
 }
