2edfd25ce95c09ba010b2764b9f185f2b6ff3af9
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-03-16 11:02:12 -0600

    add frameSkip option

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import './index.css';
 
 window.onerror = alert;
 
-const { el } = cancan({ scale: 'auto', ...gameStuff });
+const { el } = cancan({ scale: 'auto', frameSkip: 8, ...gameStuff });
 document.querySelector('body').appendChild(el);
