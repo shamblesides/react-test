f0c015c350a5215f5d517a5bf48fd84d00a146ae
Nigel Nelson <nigel@prodatakey.com>
2019-03-19 19:53:35 -0600

    remove debug text

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const frameSkip = +localStorage.getItem('frameskip') || 1;
 const { el } = cancan({ scale: 'auto', frameSkip, ...gameStuff });
 document.querySelector('body').appendChild(el);
 
-document.querySelector('body').appendChild(document.createElement('div'));
-document.querySelector('body div').id = 'debug';
+// document.querySelector('body').appendChild(document.createElement('div'));
+// document.querySelector('body div').id = 'debug';
