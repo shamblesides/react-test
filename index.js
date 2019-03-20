c7420bb778ab22e7c152edb5debbf429af9848b7
Nigel Nelson <nigel@prodatakey.com>
2019-03-19 19:27:38 -0600

    touch improvements + debugging

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const frameSkip = +localStorage.getItem('frameskip') || 1;
 
 const { el } = cancan({ scale: 'auto', frameSkip, ...gameStuff });
 document.querySelector('body').appendChild(el);
+
+document.querySelector('body').appendChild(document.createElement('div'));
+document.querySelector('body div').id = 'debug';
