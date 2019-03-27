8405d074033788b7b13254c1bf8e330d69abd978
Nigel Nelson <nigel@prodatakey.com>
2019-03-27 16:38:22 -0600

    index.html title

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './index.js',
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({ title: 'GAME' }),
         new CleanWebpackPlugin(),
     ],
     module: {
