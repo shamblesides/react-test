55c6ccfc840ffc7a51b00192051681a03e619da4
Nigel Nelson <nigel@prodatakey.com>
2019-04-19 04:02:14 -0600

    typescript defs for screen.js

---
+++ lib/screen/screen.d.ts
@@ -0,0 +1,48 @@
+type ScreenArgs = {
+    scale?: 'auto' | number = 'auto';
+    width: number = 160;
+    height: number = 144;
+    backgroundColor: string = '#000000';
+}
+
+type Sheet = {
+    src: string;
+    spriteWidth: number;
+    spriteHeight: number;
+}
+
+type BasicSprite = {
+    sheet: Sheet;
+    sprite: number;
+    x: number;
+    y: number;
+    flip: string = null;
+    colors: string[] = null;
+    cropTop: number = 0;
+    cropBottom: number = 0;
+    cropLeft: number = 0;
+    cropRight: number = 0;
+}
+
+type CompositeSprite = {
+    sprites: BasicSprite[];
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+}
+
+type FillSprite = {
+    fill: string;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+}
+
+type Sprite = BasicSprite | CompositeSprite | FillSprite;
+
+export declare function screen(args: ScreenArgs): {
+    el: HTMLCanvasElement,
+    updateScreen(sprites: Sprite[]): Promise<void>
+}
