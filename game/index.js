eba37998b7fdf00e864ed5e61edcbdc743357684
Nigel Nelson <nigel@prodatakey.com>
2019-05-03 06:25:16 -0600

    re-combined 'game' and 'screen', and make input methods optional

--- game/index.js
+++ game/index.js
@@ -2,7 +2,8 @@ import { game } from '../lib'
 import mountFullscreen from '../lib/mount-fullscreen';
 import { binds } from './binds';
 import { worldview } from './gameloop';
+import { pad } from '../lib/attach/pad'
 
 export default () => mountFullscreen(
-    game({ binds, gameloop: worldview(), height: 60, width: 90 })
+    game({ height: 60, width: 90 }, [pad(binds)], worldview())
 );
