afbd926e3910f9955dcb6a95abab8c222df260b2
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-05-04 22:40:26 -0600

    remove babel's plugin-proposal-class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,6 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-env'],
                         plugins: [
-                            require('@babel/plugin-proposal-class-properties'),
-                            require('@babel/plugin-proposal-object-rest-spread'),
                             require('@babel/plugin-syntax-dynamic-import'),
                         ],
                     },
