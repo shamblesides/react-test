5b83a0a7ecf02a987d102fecf553b76b92734fc3
Nigel Nelson <nigel@prodatakey.com>
2019-05-08 01:05:34 -0600

    reattach mount-fullscreen as a member of pxcan

--- game/index.js
+++ game/index.js
@@ -1,9 +1,6 @@
 import { pxcan } from '../lib'
-import mount from '../lib/mount-fullscreen';
 import { binds } from './binds';
 import { worldview } from './gameloop';
 import { pad } from '../lib/input/pad'
 
-export default () => mount(
-    pxcan({ height: 60, width: 90 }, [pad(binds)], worldview())
-);
+export default () => pxcan({ height: 60, width: 90 }, [pad(binds)], worldview()).fullscreen();
