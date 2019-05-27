18f164f0c8fab51dd7008c55dd1ba6e2d0a01773
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-05-27 10:45:42 -0600

    export everything from index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+export { pxcan } from './pxcan.js';
+
+// drawables
+export { fill } from './fill.js';
+export { gridSheet } from './gridsheet.js';
+export { img } from './img.js';
+export { letters } from './letters.js';
+export { pane } from './pane.js';
+
+// transforms
+export { crop } from './crop.js';
+export { flip } from './flip.js';
+export { recolor } from './recolor.js';
+
+// inputs
+export { pad } from './pad.js';
+export { pointer } from './pointer.js';
